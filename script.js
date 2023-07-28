8d5f3591ba06ab81b00c7cce51606157c06c4f1c
ratsaregreat <140848517+ratsaregreat@users.noreply.github.com>
2023-07-28 12:06:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openLink() {
+ window.open("https://www.youtube.com/watch?v=AhYitvMEukc");
+}
+
+function clicker_button() {
+ document.getElementById("clicker").scrollIntoView();
+}
+function about_button() {
+ document.getElementById("About").scrollIntoView();
+}
+function Rat_button() {
+ document.getElementById("Rat").scrollIntoView();
+}
